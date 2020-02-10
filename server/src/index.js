99a417ce1eec824dd07e281f0e19f51369daf8bc
wptechprodigy <waheed.freelancer16@gmail.com>
2020-02-10 17:15:13 +0100

    add some middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
+// Express app setup
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(`Express is running on port: ${port}`)
-})
+// Middlewares
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
+// app.disable('x-powered-by')
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World!'
+  });
+});
+
+// Port definition
 const port = process.env.PORT || 1980;
 
 app.listen(port, () => {
