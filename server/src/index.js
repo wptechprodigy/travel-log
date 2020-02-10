e7e27eaaf54fa9c8086e5fb02461845f36973731
wptechprodigy <waheed.freelancer16@gmail.com>
2020-02-10 17:35:24 +0100

    setup error handler middleware

--- server/src/index.js
+++ server/src/index.js
@@ -21,6 +21,22 @@ app.get('/', (req, res) => {
   });
 });
 
+// If all routes are not found - error handler
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    error: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
+  })
+})
+
 // Port definition
 const port = process.env.PORT || 1980;
 
