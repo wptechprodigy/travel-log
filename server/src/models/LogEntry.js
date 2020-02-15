14babe6f66ae012b281f39964dd2abda12822d14
wptechprodigy <waheed.freelancer16@gmail.com>
2020-02-15 17:07:18 +0100

    prettier format

--- server/src/models/LogEntry.js
+++ server/src/models/LogEntry.js
@@ -3,38 +3,41 @@ const Schema = mongoose.Schema;
 
 const requiredNumber = {
   type: Number,
-  required: true
-}
+  required: true,
+};
 
-const logEntrySchema = new Schema({
-  title:  {
-    type: String,
-    required: true
+const logEntrySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    comments: String,
+    image: String,
+    visitedDate: {
+      type: Date,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      min: 0,
+      max: 10,
+      default: 0,
+    },
+    latitude: {
+      ...requiredNumber,
+      min: -90,
+      max: 90,
+    },
+    longitude: {
+      ...requiredNumber,
+      min: -180,
+      max: 180,
+    },
   },
-  description: String,
-  comments: String,
-  image: String,
-  visitedDate: {
-    type: Date,
-    required: true
-  },
-  Rating: {
-    type: Number,
-    min: 0,
-    max: 10,
-    default: 0
-  },
-  latitude: {
-    ...requiredNumber,
-    min: -90,
-    max: 90,
-  },
-  longitude: {
-    ...requiredNumber,
-    min: -180,
-    max: 180,
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const LogEntry = mongoose.model('LogEntry', logEntrySchema);
 
