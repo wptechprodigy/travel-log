26392677b56b638fd613f451586784faff00693b
wptechprodigy <waheed.freelancer16@gmail.com>
2020-02-16 11:19:56 +0100

    add a marker to spot location on map

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,20 @@ function App() {
           offsetLeft={-20}
           offsetTop={-10}
         >
-          <div>{entry.title}</div>
+          <svg
+            className="marker"
+            viewBox="0 0 24 24"
+            width="28"
+            height="28"
+            stroke="currentColor"
+            stroke-width="2"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+            <circle cx="12" cy="10" r="3"></circle>
+          </svg>
         </Marker>
       ))}
     </ReactMapGL>
