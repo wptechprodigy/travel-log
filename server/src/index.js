bd1f6cf6325580a25344f666e6d304132f7a6cf0
wptechprodigy <waheed.freelancer16@gmail.com>
2020-04-17 05:48:15 +0100

    watch change

--- server/src/index.js
+++ server/src/index.js
@@ -63,9 +63,9 @@ const port = process.env.PORT || 1980;
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // })
 }
 
 app.listen(port, () => {
